f870f5e6778c40b09784a645b966d9f1cace41d2
David Lee <dcl@nevoweb.com>
2018-12-10 11:57:35 +0100

    upd

--- API/render/DNNrocketTokens.cs
+++ API/render/DNNrocketTokens.cs
@@ -1,4 +1,5 @@
 ﻿using NBrightDNN.render;
+using RazorEngine.Text;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,8 +8,36 @@ using System.Threading.Tasks;
 
 namespace DNNrocketAPI.render
 {
-    public class DNNrocketTokens<T> : RazorEngineTokens<T>
+    public class DNNrocketTokens<T> : Simplisity.RazorEngineTokens<T>
     {
+
+        public IEncodedString ResourceKey(String resourceFileKey, String lang = "", String resourceExtension = "Text")
+        {
+            return new RawString(ResourceKeyString(resourceFileKey, lang, resourceExtension));
+        }
+
+        public IEncodedString ResourceKeyJS(String resourceFileKey, String lang = "", String resourceExtension = "Text")
+        {
+            var strOut = ResourceKeyString(resourceFileKey, lang, resourceExtension);
+            strOut = strOut.Replace("'", "\\'");
+            return new RawString(strOut);
+        }
+
+        private string ResourceKeyString(String resourceFileKey, String lang = "", String resourceExtension = "Text")
+        {
+            var strOut = "";
+            if (Metadata.ContainsKey("resourcepath"))
+            {
+                var l = Metadata["resourcepath"];
+                foreach (var r in l)
+                {
+                    strOut = DNNrocketUtils.GetResourceString(r, resourceFileKey, resourceExtension, lang);
+                    if (strOut != "") break;
+                }
+            }
+            return strOut;
+        }
+
 
     }
 }
