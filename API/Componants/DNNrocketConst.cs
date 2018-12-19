d38188f6e464e0cc83de376b5ee81b87632bbd3f
leedavi <dcl@nevoweb.com>
2018-12-19 04:49:02 +0100

    use table

--- API/Componants/DNNrocketConst.cs
+++ API/Componants/DNNrocketConst.cs
@@ -8,9 +8,11 @@ namespace DNNrocketAPI
 {
     public static class DNNrocketRoles
     {
-        public const String ManagerRole = "Manager";
-        public const String EditorRole = "Editor";
-        public const String ClientEditorRole = "ClientEditor";
+        public const String Administrator = "Administrators";
+        public const String Manager = "Manager";
+        public const String Editor = "Editor";
+        public const String ClientEditor = "ClientEditor";
+        public const String Premium = "Premium";
     }
 
 }
