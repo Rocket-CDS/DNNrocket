90c9e9738e54883197e9a47f1c80a04772c42e9a
leedavi <dcl@nevoweb.com>
2019-10-13 15:36:45 +0200

    upd

--- API/Componants/UserStorage.cs
+++ API/Componants/UserStorage.cs
@@ -20,22 +20,38 @@ namespace DNNrocketAPI.Componants
 
             _guidKey = _entityTypeCode + "*" + UserId;
             _objCtrl = new DNNrocketController();
-            Record = _objCtrl.GetRecordByGuidKey(-1, -1, _entityTypeCode, _guidKey, UserId.ToString());
+
+            Record = (SimplisityRecord)CacheUtils.GetCache(_guidKey);
             if (Record == null)
             {
-                var Record = new SimplisityRecord();
-                Record.PortalId = -1;
-                Record.ModuleId = -1;
-                Record.TypeCode = _entityTypeCode;
-                Record.GUIDKey = _guidKey;
-                Record.UserId = UserId;
-                _objCtrl.SaveRecord(Record, 0,_tableName);
+                Record = _objCtrl.GetRecordByGuidKey(-1, -1, _entityTypeCode, _guidKey, UserId.ToString());
+                if (Record == null)
+                {
+                    Record = new SimplisityRecord();
+                    Record.PortalId = -1;
+                    Record.ModuleId = -1;
+                    Record.TypeCode = _entityTypeCode;
+                    Record.GUIDKey = _guidKey;
+                    Record.UserId = UserId;
+                    _objCtrl.SaveRecord(Record, 0, _tableName);
+                }
+                CacheUtils.SetCache(_guidKey, Record);
             }
         }
 
         public void Save()
         {
             _objCtrl.SaveRecord(Record);
+            CacheUtils.SetCache(_guidKey, Record);
+        }
+        public void Delete()
+        {
+            _objCtrl.Delete(Record.ItemID);
+            ClearCache();
+        }
+        public void ClearCache()
+        {
+            CacheUtils.RemoveCache(_guidKey);
         }
 
         public void Set(string nodename,string value)
