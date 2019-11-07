bab15a42729d782faaf873d8df960097cfd06f0c
David Lee <dcl@nevoweb.com>
2019-11-07 12:13:18 +0100

    upd

--- API/Componants/SystemGlobalData.cs
+++ API/Componants/SystemGlobalData.cs
@@ -9,39 +9,46 @@ namespace DNNrocketAPI.Componants
 {
     public class SystemGlobalData
     {
-        private static DNNrocketController _objCtrl;
+        private static string _cacheKey; 
         public SystemGlobalData()
         {
-            _objCtrl = new DNNrocketController();
-            LoadData();
+            _cacheKey = "GLOBALSETTINGS";
+            Info = (SimplisityInfo)CacheUtils.GetCache(_cacheKey);
+            if (Info == null)
+            {
+                LoadData();
+            }
         }
 
         public void Save(SimplisityInfo postInfo)
         {
+             var objCtrl = new DNNrocketController();
             Info.XMLData = postInfo.XMLData;
-            _objCtrl.Update(Info);
+            objCtrl.Update(Info);
         }
 
         private void LoadData()
         {
-            Info = _objCtrl.GetByType(DNNrocketUtils.GetPortalId(), -1, "GLOBALSETTINGS");
+            var objCtrl = new DNNrocketController();
+            Info = objCtrl.GetByType(DNNrocketUtils.GetPortalId(), -1, "GLOBALSETTINGS");
             if (Info == null)
             {
                 Info = new SimplisityInfo();
                 Info.ItemID = -1;
                 Info.PortalId = DNNrocketUtils.GetPortalId();
                 Info.TypeCode = "GLOBALSETTINGS";
-                Info.ItemID = _objCtrl.Update(Info);
+                Info.ItemID = objCtrl.Update(Info);
             }
         }
 
         public SimplisityInfo Info { get; set; }
 
-        public string FtpUserName { get { return Info.GetXmlProperty("genxml/textbox/ftpuser"); } }
-        public string FtpPassword { get { return Info.GetXmlProperty("genxml/textbox/ftppassword"); } }
-        public string FtpServer { get { return Info.GetXmlProperty("genxml/textbox/ftpserver"); } }
-        public string PublicAppThemeURI { get { return Info.GetXmlProperty("genxml/textbox/publicappthemeuri"); } }
-
+        public string FtpUserName { get { return Info.GetXmlProperty("genxml/textbox/ftpuser"); } set { Info.SetXmlProperty("genxml/textbox/ftpuser",value); } }
+        public string FtpPassword { get { return Info.GetXmlProperty("genxml/textbox/ftppassword"); } set { Info.SetXmlProperty("genxml/textbox/ftppassword", value); } }
+        public string FtpServer { get { return Info.GetXmlProperty("genxml/textbox/ftpserver"); } set { Info.SetXmlProperty("genxml/textbox/ftpserver", value); } }
+        public string PublicAppThemeURI { get { return Info.GetXmlProperty("genxml/textbox/publicappthemeuri"); } set { Info.SetXmlProperty("genxml/textbox/publicappthemeuri", value); } }
+        public string ImageType { get { return Info.GetXmlProperty("genxml/select/imagetype"); } set { Info.SetXmlProperty("genxml/select/imagetype", value); } }
+        public bool PngImage { get { if (Info.GetXmlProperty("genxml/select/imagetype") != "jpg") return true; else return false; } }
 
     }
 }
