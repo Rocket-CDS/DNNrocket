12b8c355f26209ae25c38795b2963ca506c251e5
leedavi <dcl@nevoweb.com>
2019-12-31 18:06:48 +0100

    upd

---
+++ API/Componants/ArchiveData.cs
@@ -0,0 +1,51 @@
+﻿using Simplisity;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace DNNrocketAPI.Componants
+{
+    public class ArchiveData
+    {
+
+        public ArchiveData(string fileMapPath)
+        {
+            Exists = false;
+            FileMapPath = fileMapPath;
+            if (File.Exists(FileMapPath))
+            {
+                Exists = true;
+                var archiveXml = FileUtils.ReadFile(fileMapPath);
+                Info = new SimplisityInfo(archiveXml);
+                var nodList = Info.XMLDoc.SelectNodes("root/*");
+                foreach (XmlNode nod in nodList)
+                {
+                    var importXml = nod.OuterXml;
+                    var importInfo = new SimplisityInfo();
+                    importInfo.FromXmlItem(importXml);
+                    ItemList.Add(importInfo);
+                }
+            }
+        }
+
+        public void RestoreData(string tableName = "DNNrocket")
+        {
+            var objCtrl = new DNNrocketController();
+            foreach (var s in ItemList)
+            {
+                objCtrl.Update(s, tableName);
+            }
+        }
+
+        public List<SimplisityInfo> ItemList { get; set; }
+        public string FileMapPath { get; set; }
+        public bool Exists { get; set; }
+        public SimplisityInfo Info { get; set; }
+
+
+    }
+}
---
+++ API/Componants/ArchiveDataList.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DNNrocketAPI.Componants
+{
+    public class ArchiveDataList
+    {
+        
+        public ArchiveDataList()
+        {
+            LoadData();
+        }
+
+        private void LoadData()
+        {
+            List = new Dictionary<string, List<ArchiveData>>();
+            ModuleFolderList = new List<string>();
+
+            var l = Directory.GetDirectories(DNNrocketUtils.ArchiveDirectoryMapPath());
+            foreach (var d in l)
+            {
+                string fullPath = Path.GetFullPath(d).TrimEnd(Path.DirectorySeparatorChar);
+                string projectName = Path.GetFileName(fullPath);
+
+                var fileList = new List<ArchiveData>();
+                ModuleFolderList.Add(projectName);
+                var l2 = Directory.GetFiles(d);
+                foreach (var f in l2)
+                {
+                    var archiveData = new ArchiveData(f);
+                    fileList.Add(archiveData);
+                }
+                List.Add(projectName, fileList);
+            }
+        }
+
+        public List<ArchiveData> GetArchiveList(string moduleFolder)
+        {
+            if (List.ContainsKey(moduleFolder))
+            {
+                return List[moduleFolder];
+            }
+            return null;
+        }
+
+        public Dictionary<string, List<ArchiveData>> List { get; set; }
+        public List<string> ModuleFolderList { get; set; }
+
+    }
+}
