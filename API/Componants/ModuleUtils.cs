e3de490a1fda7b29b45f3e7a67e7fcdb1f19da67
leedavi <dcl@nevoweb.com>
2020-06-22 12:42:00 +0200

    upd

---
+++ API/Componants/ModuleUtils.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Simplisity;
+using System.IO;
+
+namespace DNNrocketAPI.Componants
+{
+    public static class ModuleUtils
+    {
+        public static void CraeteModuleFromManifest()
+        {
+
+
+        }
+
+
+    }
+}
