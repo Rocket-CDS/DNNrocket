1c5e1f7dd7a0e43b2fd55c08653b6911cc4a622b
leedavi <dcl@agence-sesame.fr>
2020-12-16 15:16:33 +0100

    Remove Ants  - Components

--- API/Components/UserLimpetList.cs
+++ API/Components/UserLimpetList.cs
@@ -8,7 +8,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace RocketEcommerce.Componants
+namespace RocketEcommerce.Components
 {
     public class UserLimpetList
     {
