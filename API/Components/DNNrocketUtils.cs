4a21e642dbdb6a303150295ed1660b3d95824001
leedavi <dcl@agence-sesame.fr>
2021-04-01 16:05:51 +0200

    unique image name

--- API/Components/DNNrocketUtils.cs
+++ API/Components/DNNrocketUtils.cs
@@ -1035,15 +1035,17 @@ namespace DNNrocketAPI.Components
             return rtn;
         }
 
-        public static string GetUniqueFileName(string fileName, string folderMapPath, int idx = 1)
+        public static string GetUniqueFileName(string fileName, string folderMapPath, int idx = 1, string originalFileName = "")
         {
+            if (originalFileName == "") originalFileName = fileName;
+
             foreach (char c in System.IO.Path.GetInvalidFileNameChars())
             {
                 fileName = fileName.Replace(c, '_');
             }
             if (File.Exists(folderMapPath + "\\" + fileName))
             {
-                fileName = GetUniqueFileName(Path.GetFileNameWithoutExtension(fileName) + idx + Path.GetExtension(fileName), folderMapPath, idx + 1);
+                fileName = GetUniqueFileName(Path.GetFileNameWithoutExtension(originalFileName) + idx + Path.GetExtension(originalFileName), folderMapPath, idx + 1, originalFileName);
             }
             return fileName;
         }
