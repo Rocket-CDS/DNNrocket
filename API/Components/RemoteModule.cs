1a2b6829002d2c59646fc7649eb9f77e450621f7
leedavi <dcl@agence-sesame.fr>
2021-10-18 09:47:03 +0200

    remotemodule

---
+++ API/Components/RemoteModule.cs
@@ -0,0 +1,76 @@
+﻿using Simplisity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace DNNrocketAPI.Components
+{
+    public class RemoteModule
+    {
+        private DNNrocketController _objCtrl;
+        private const string _tableName = "DNNrocket";
+        RemoteModule(string moduleRef)
+        {
+            _objCtrl = new DNNrocketController();
+
+
+        }
+        public int Save(SimplisityInfo postInfo)
+        {
+            ReplaceInfoFields(postInfo, "genxml/textbox/*");
+            ReplaceInfoFields(postInfo, "genxml/lang/genxml/textbox/*");
+            ReplaceInfoFields(postInfo, "genxml/checkbox/*");
+            ReplaceInfoFields(postInfo, "genxml/select/*");
+            ReplaceInfoFields(postInfo, "genxml/radio/*");
+
+            return ValidateAndUpdate();
+        }
+        public int Update()
+        {
+            Record = _objCtrl.SaveRecord(Record, _tableName);
+            return Record.ItemID;
+        }
+
+        private void ReplaceInfoFields(SimplisityInfo postInfo, string xpathListSelect)
+        {
+            var textList = Record.XMLDoc.SelectNodes(xpathListSelect);
+            if (textList != null)
+            {
+                foreach (XmlNode nod in textList)
+                {
+                    Record.RemoveXmlNode(xpathListSelect.Replace("*", "") + nod.Name);
+                }
+            }
+            textList = postInfo.XMLDoc.SelectNodes(xpathListSelect);
+            if (textList != null)
+            {
+                foreach (XmlNode nod in textList)
+                {
+                    Record.SetXmlProperty(xpathListSelect.Replace("*", "") + nod.Name, nod.InnerText);
+                }
+            }
+        }
+
+
+
+        #region "properties"
+
+        public string EntityTypeCode { get { return "RMOD"; } }
+        public SimplisityRecord Record { get; set; }
+        public int ModuleId { get { return Record.ModuleId; } set { Record.ModuleId = value; } }
+        public int XrefItemId { get { return Record.XrefItemId; } set { Record.XrefItemId = value; } }
+        public int ParentItemId { get { return Record.ParentItemId; } set { Record.ParentItemId = value; } }
+        public int ItemId { get { return Record.ItemID; } set { Record.ItemID = value; } }
+        public string ModuleRef { get { return Record.GUIDKey; } set { Record.GUIDKey = value; } }
+        public string GUIDKey { get { return Record.GUIDKey; } set { Record.GUIDKey = value; } }
+        public int SortOrder { get { return Record.SortOrder; } set { Record.SortOrder = value; } }
+        public int PortalId { get { return Record.PortalId; } }
+        public bool Exists { get { if (Record.ItemID <= 0) { return false; } else { return true; }; } }
+
+        #endregion
+
+    }
+}
