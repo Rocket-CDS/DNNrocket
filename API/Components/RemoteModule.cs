945f03726edf6c0d9a0cd3446af7530ca71ade3e
leedavi <dcl@agence-sesame.fr>
2021-10-25 12:01:25 +0200

    remotemodule class

--- API/Components/RemoteModule.cs
+++ API/Components/RemoteModule.cs
@@ -52,10 +52,10 @@ namespace DNNrocketAPI.Components
         public int PortalId { get { return Record.PortalId; } }
         public bool Exists { get { if (Record.ItemID <= 0) { return false; } else { return true; }; } }
 
-        public string AppThemeFolderView { get { if (Record.GetXmlProperty("genxml/select/appthemeview") == "") return AppThemeFolder; else return Record.GetXmlProperty("genxml/select/appthemefolderview"); } set { Record.SetXmlProperty("genxml/settings/appthemeview", value); } }
-        public string AppThemeVersionView { get { if (Record.GetXmlProperty("genxml/select/appthemeversionview") == "") return AppThemeVersion; else return Record.GetXmlProperty("genxml/select/appthemeversionview"); } set { Record.SetXmlProperty("genxml/settings/appthemeversionview", value); } }
-        public string AppThemeFolder { get { return Record.GetXmlProperty("genxml/select/apptheme"); } set { Record.SetXmlProperty("genxml/select/apptheme", value); } }
-        public string AppThemeVersion { get { return Record.GetXmlProperty("genxml/select/appthemeversion"); } set { Record.SetXmlProperty("genxml/select/appthemeversion", value); } }
+        public string AppThemeFolderView { get { if (Record.GetXmlProperty("genxml/remote/appthemeview") == "") return AppThemeFolder; else return Record.GetXmlProperty("genxml/remote/appthemefolderview"); } set { Record.SetXmlProperty("genxml/remote/appthemeview", value); } }
+        public string AppThemeVersionView { get { if (Record.GetXmlProperty("genxml/remote/appthemeversionview") == "") return AppThemeVersion; else return Record.GetXmlProperty("genxml/remote/appthemeversionview"); } set { Record.SetXmlProperty("genxml/remote/appthemeversionview", value); } }
+        public string AppThemeFolder { get { return Record.GetXmlProperty("genxml/remote/apptheme"); } set { Record.SetXmlProperty("genxml/select/apptheme", value); } }
+        public string AppThemeVersion { get { return Record.GetXmlProperty("genxml/remote/appthemeversion"); } set { Record.SetXmlProperty("genxml/select/appthemeversion", value); } }
 
         #endregion
 
