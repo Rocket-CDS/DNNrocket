43081a7aa7d0a11b4a9227bd00700c1ce293abbd
David Lee <dcl@agence-sesame.fr>
2022-05-22 10:04:07 +0200

    remote langauge selector

--- API/render/DNNrocketTokens.cs
+++ API/render/DNNrocketTokens.cs
@@ -148,6 +148,12 @@ namespace DNNrocketAPI.render
             model.SetSetting("sfields", sfields);
             return RenderTemplate("RemoteLanguageChange.cshtml", appThemeSystem, model, true);
         }
+        public IEncodedString RenderRemoteLanguageSelector(string scmd, string sfields, AppThemeDNNrocketLimpet appThemeDNNrocket, SimplisityRazor model)
+        {
+            model.SetSetting("scmd", scmd);
+            model.SetSetting("sfields", sfields);
+            return RenderTemplate("RemoteLanguageChange.cshtml", appThemeDNNrocket, model, true);
+        }
 
         public IEncodedString RenderPagingTemplate(string scmd, string spost, SimplisityRazor model, string sreturn = "", string versionFolder = "1.0")
         {
