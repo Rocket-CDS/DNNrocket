ab02f2ef47a15539406d677aa371d070dd6a38e4
leedavi <dcl@agence-sesame.fr>
2022-07-14 17:10:20 +0200

    hbs plugin

---
+++ API/Interfaces/HandleBarsInterface.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Remoting;
+using Simplisity;
+using DNNrocketAPI.Components;
+using HandlebarsDotNet;
+
+namespace DNNrocketAPI
+{
+
+    public abstract class HandleBarsInterface
+    {
+        static HandleBarsInterface()
+		{
+		}
+        public static HandleBarsInterface Instance(string assembly, string namespaceclass)
+        {
+            var handle = Activator.CreateInstance(assembly, namespaceclass);
+            return (HandleBarsInterface)handle.Unwrap();
+        }
+        public abstract void RegisterHelpers(ref IHandlebars hbs);
+    }
+
+}
+
