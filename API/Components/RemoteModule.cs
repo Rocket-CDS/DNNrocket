e79f00afd4daaaae6e68cb43dd97af3fe6a67060
David Lee <dcl@agence-sesame.fr>
2022-08-13 11:50:01 +0200

    org --> project

--- API/Components/RemoteModule.cs
+++ API/Components/RemoteModule.cs
@@ -71,7 +71,7 @@ namespace DNNrocketAPI.Components
         public string ModuleName { get { return Record.GetXmlProperty("genxml/remote/modulename"); } set { Record.SetXmlProperty("genxml/remote/modulename", value); } }
         public string DataRef { get { if (Record.GetXmlProperty("genxml/remote/dataref") == "") return ModuleRef; else return Record.GetXmlProperty("genxml/remote/dataref"); } set { Record.SetXmlProperty("genxml/remote/dataref", value); } }
         public string SiteKey { get { return Record.GetXmlProperty("genxml/remote/sitekey"); } set { Record.SetXmlProperty("genxml/remote/sitekey", value); } }
-        public string Organisation { get { return Record.GetXmlProperty("genxml/remote/org"); } }
+        public string ProjectName { get { return Record.GetXmlProperty("genxml/remote/projectname"); } }
         public string TemplateSelected { get { return Record.GetXmlProperty("genxml/remote/template"); } }
         public string TemplateSelectedSettings { get { return Path.GetFileNameWithoutExtension(TemplateSelected) + "-Settings" + Path.GetExtension(TemplateSelected); } }
 
