c43ae5699a46c2047e3ffb4a49b36d67c8c99c4f
leedavi <dcl@agence-sesame.fr>
2023-06-15 14:38:47 +0200

    RocketTools - Clone

--- RocketTools/render/RocketToolsTokens.cs
+++ RocketTools/render/RocketToolsTokens.cs
@@ -1,6 +1,8 @@
 ﻿using DNNrocketAPI;
 using DNNrocketAPI.Components;
+using DotNetNuke.Entities.Tabs;
 using RazorEngine.Text;
+using RocketTools.Components;
 using Simplisity;
 using System;
 using System.Collections.Generic;
@@ -15,6 +17,48 @@ namespace RocketTools
 {
     public class RocketToolsTokens<T> : DNNrocketAPI.render.DNNrocketTokens<T>
     {
+        public IEncodedString GetTabList(int portalId, List<int> selectedTabIdList, string treeviewId, string lang = "", string attributes = "", bool showAllTabs = false)
+        {
+            if (lang == "") lang = DNNrocketUtils.GetCurrentCulture();
+            var tabList = TabController.GetTabsBySortOrder(portalId, lang, true);
+            var rtnString = "";
+            var strOut = GetTabList(rtnString, tabList, 0, 0, treeviewId, attributes, selectedTabIdList, showAllTabs);
+            strOut += "";
+            return new RawString(strOut);
+        }
+        private static string GetTabList(string rtnString, List<TabInfo> tabList, int level, int parentid, string id, string attributes, List<int> selectedTabIdList, bool showAllTabs)
+        {
+
+            if (level > 50) // stop infinate loop
+            {
+                return rtnString;
+            }
+
+            foreach (TabInfo tInfo in tabList)
+            {
+                var parenttestid = tInfo.ParentId;
+                if (parenttestid < 0) parenttestid = 0;
+                if (parentid == parenttestid)
+                {
+                    var checkedvalue = "";
+                    if (selectedTabIdList.Contains(tInfo.TabID)) checkedvalue = "checked";
+
+                    rtnString += "<div>";
+                    for (int i = 0; i < level; i++)
+                    {
+                        rtnString += "&nbsp;&nbsp;";
+                    }
+                    rtnString += "<input id='tabid-" + id + "-" + tInfo.TabID + "' data-id='" + tInfo.TabID + "' s-xpath='genxml/treeview/" + id + "/tabid" + tInfo.TabID + "' s-update='save' " + checkedvalue + " type='checkbox'>";
+                    rtnString += tInfo.TabName;
+                    rtnString += "</div>";
+                    if (tInfo.HasChildren)
+                    {
+                        rtnString = GetTabList(rtnString, tabList, level + 1, tInfo.TabID, id, attributes, selectedTabIdList, showAllTabs);
+                    }
+                }
+            }
+            return rtnString;
+        }
 
 
     }
