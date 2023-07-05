68d8a27e9ce5fef913e6379a32938c000f43a369
leedavi <dcl@agence-sesame.fr>
2023-07-05 06:24:20 +0200

    fix webp thumbnailer

--- API/Components/ImgUtils.cs
+++ API/Components/ImgUtils.cs
@@ -608,6 +608,7 @@ namespace DNNrocketAPI.Components
             try
             {
                 Bitmap sourceImage;
+                if (imgtype == "") imgtype = Path.GetExtension(strFilepath).Trim('.');
                 if (imgtype == "webp")
                 {
                     try
