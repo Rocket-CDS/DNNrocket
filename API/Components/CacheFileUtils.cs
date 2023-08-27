47f98dd6dfeda644a883e503b2d73117ffd8b837
leedavi <dcl@agence-sesame.fr>
2023-08-27 07:12:29 +0200

    write binary to file (for cache)

--- API/Components/CacheFileUtils.cs
+++ API/Components/CacheFileUtils.cs
@@ -1,5 +1,6 @@
 ﻿using DNNrocketAPI;
 using DNNrocketAPI.Components;
+using Newtonsoft.Json.Linq;
 using Simplisity;
 using System;
 using System.Collections.Generic;
@@ -115,5 +116,47 @@ namespace DNNrocketAPI.Components
 
         #endregion
 
+        #region "Binary"
+
+        /// <summary>
+        /// Writes the given object instance to a binary file.
+        /// <para>Object type (and all child types) must be decorated with the [Serializable] attribute.</para>
+        /// <para>To prevent a variable from being serialized, decorate it with the [NonSerialized] attribute; cannot be applied to properties.</para>
+        /// </summary>
+        /// <typeparam name="T">The type of object being written to the XML file.</typeparam>
+        /// <param name="filePath">The file path to write the object instance to.</param>
+        /// <param name="objectToWrite">The object instance to write to the XML file.</param>
+        /// <param name="append">If false the file will be overwritten if it already exists. If true the contents will be appended to the file.</param>
+        public static void WriteToBinaryFile<T>(string cacheKey, T objectToWrite, bool append = false)
+        {
+            var cacheFile = GetMd5Hash(cacheKey);
+            var filePath = PortalUtils.TempDirectoryMapPath().Trim('\\') + "\\cache\\" + cacheFile;
+
+            using (Stream stream = File.Open(filePath, append ? FileMode.Append : FileMode.Create))
+            {
+                var binaryFormatter = new System.Runtime.Serialization.Formatters.Binary.BinaryFormatter();
+                binaryFormatter.Serialize(stream, objectToWrite);
+            }
+        }
+
+        /// <summary>
+        /// Reads an object instance from a binary file.
+        /// </summary>
+        /// <typeparam name="T">The type of object to read from the XML.</typeparam>
+        /// <param name="filePath">The file path to read the object instance from.</param>
+        /// <returns>Returns a new instance of the object read from the binary file.</returns>
+        public static T ReadFromBinaryFile<T>(string cacheKey)
+        {
+            var cacheFile = GetMd5Hash(cacheKey);
+            var filePath = PortalUtils.TempDirectoryMapPath().Trim('\\') + "\\cache\\" + cacheFile;
+
+            using (Stream stream = File.Open(filePath, FileMode.Open))
+            {
+                var binaryFormatter = new System.Runtime.Serialization.Formatters.Binary.BinaryFormatter();
+                return (T)binaryFormatter.Deserialize(stream);
+            }
+        }
+
+        #endregion
     }
 }
