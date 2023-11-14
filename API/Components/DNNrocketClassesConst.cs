6f1885a01ccb4609fec4a29e34b5737d879796fc
leedavi <dcl@agence-sesame.fr>
2023-11-14 07:15:19 +0100

    link icon

--- API/Components/DNNrocketClassesConst.cs
+++ API/Components/DNNrocketClassesConst.cs
@@ -16,7 +16,7 @@ namespace DNNrocketAPI.Components
         public const string Premium = "Premium";
     }
 
-    public enum ButtonTypes { add, admin, back, cancel, close, create, delete, download, edit, export, execute, import, next, ok, print, refresh, remove, reset, save, search, cancelsearch, setup, send, undo, upload, use, previous, yes, no, fail, bars, copy, paste, home, photo, folder, folderopen, tree, notes, history, locked, unlocked, task, savesend, settings, translate };
+    public enum ButtonTypes { add, admin, back, cancel, close, create, delete, download, edit, export, execute, import, next, ok, print, refresh, remove, reset, save, search, cancelsearch, setup, send, undo, upload, use, previous, yes, no, fail, bars, copy, paste, home, photo, folder, folderopen, tree, notes, history, locked, unlocked, task, savesend, settings, translate, link };
 
 
     public class ValuePair
