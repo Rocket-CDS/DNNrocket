069daa29bed33aa9120657bb774fcb7d991abc87
David Lee <dcl@agence-sesame.fr>
2025-02-12 13:45:52 +0100

    ckeditor disable versionCheck

--- API/Themes/config-w3/1.0/js/ckeditor4startup1.js
+++ API/Themes/config-w3/1.0/js/ckeditor4startup1.js
@@ -4,8 +4,10 @@ $(document).ready(function () {
 
 	CKEDITOR.replace('{id}', {
 		customConfig: '/DesktopModules/DNNrocket/API/Themes/config-w3/1.0/js/ckeditor4config1.js',
-		extraPlugins: 'justify'
-    });
+		extraPlugins: 'justify',
+		// Disable security notifications
+		versionCheck: false
+	});
 
 	CKEDITOR.instances.{id}.on('key', function () {
 		$('#{id}').val(CKEDITOR.instances.{id}.getData());
