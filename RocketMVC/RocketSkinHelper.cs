449e06957487c072147588c1aa4c3c814c9ea1bb
David Lee <dcl@agence-sesame.fr>
2025-05-06 06:06:50 +0200

    RocketMVC

---
+++ RocketMVC/RocketSkinHelper.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Web;
+using System.Web.Mvc;
+using DotNetNuke.Web.MvcPipeline.Models;
+
+namespace DNNrocket.MVC
+{
+    public static class RocketSkinHelper
+    {
+        public static MvcHtmlString Testing() 
+        {
+            return new MvcHtmlString("<h1>testing RocketSkinHelper</h1>"); 
+        }
+    }
+}
