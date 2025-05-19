072682da4ba868a2960b670f5015dfce1d92c7f2
David Lee <dcl@agence-sesame.fr>
2025-05-19 08:24:14 +0200

    RocketEndInstall

---
+++ RocketEndInstall/Dnn.PersonaBar/Modules/RocketCDS/scripts/RocketCDS.js
@@ -0,0 +1,19 @@
+'use strict';
+define(['jquery'],
+    function () {
+ 
+        return {
+            init: function (wrapper, util, params, callback) {
+            },
+ 
+            initMobile: function (wrapper, util, params, callback) {
+            },
+ 
+            load: function (params, callback) {
+ 
+            },
+ 
+            loadMobile: function (params, callback) {
+            }
+        };
+    });
---
+++ RocketEndInstall/RocketEndInstall.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace RocketEndInstall
+{
+    public class RocketEndInstall
+    {
+
+    }
+}
